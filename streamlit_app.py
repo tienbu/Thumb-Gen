a847f7feae20b7e960054dd61ed547d88a1cc058
tienbu <tienbu@gmail.com>
2025-05-23 17:25:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ if view == "Account":
             2. Click your profile icon (top left) → **Settings**.
             3. Under **Security & access**, find **Personal API keys**.
             4. Click **New API key**, give it a name, and copy the key.
-            5. ""Important"" This key will only be shown once. Save it somewhere safe!
+            5. **Important** This key will only be shown once. Save it somewhere safe!
             5. Paste the API key below.
             """
         )
